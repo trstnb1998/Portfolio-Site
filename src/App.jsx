61577786aec6ad90ba4d540675c56c098bf80ca4
Tristan <Tristanbartolome@hotmail.com>
2023-09-01 23:54:46 +1000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn
-      </p>
     </>
   )
 }

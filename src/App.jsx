8b986a602c44ccb79521247009e67f91f783c7f8
trstnb1998 <tristanbartolome@hotmail.com>
2023-09-14 17:10:57 +1000

    starry backdrop added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import NavBar from './components/NavBar/NavBar'
+import StarBackdrop from './components/Backdrop/StarBackdrop'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <div>
+
       <div>
+        <StarBackdrop />
         <NavBar />
       </div>
-      
       <h1>HEY THERE,</h1>
       <h1>MY NAME IS TRISTAN!</h1>
       <h2>Junior Software Engineer</h2>
       <h2>Take a look around my website if you would like to get to know a bit about me!</h2>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Backdrop/StarBackdrop.jsx
@@ -0,0 +1,13 @@
+import '../Backdrop/StarBackdrop.css'
+
+export default function StarBackdrop() {
+    return (
+
+        <div className="bg-animation">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div id="stars4"></div>
+        </div>
+    );
+}

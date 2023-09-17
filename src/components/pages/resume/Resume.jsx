036363e455681f7acd79a7bcf3a22a60d4787839
Tristan <Tristanbartolome@hotmail.com>
2023-09-18 00:42:36 +1000

    Resume added

--- src/components/pages/resume/Resume.jsx
+++ src/components/pages/resume/Resume.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './Resume.css'
+import ResumeImage from '/src/assets/ResumeImage.jpg'
+import ResumePDF from '/src/assets/ResumePDF.pdf'
 
 export default function Resume() {
-  return (
-    <div>Resume</div>
-  )
+    return (
+        <div className='container1'>
+            <a href={ResumePDF} download='ResumePDF' target='_blank' className='PDF-link'>
+                <button className='PDF-button'>DOWNLOAD PDF</button>
+            </a>
+            <img className='resume-img' src={ResumeImage} alt="Resume" />
+        </div>
+    )
 }

a2d6a10558939a0e083b798384aa4ae700e45209
Tristan <Tristanbartolome@hotmail.com>
2023-09-19 15:05:52 +1000

    typing animation added

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../about/About.css'
+import { TypeAnimation } from 'react-type-animation';
 import chopper from '/src/assets/chopper.gif'
+import deskstudy from '/src/assets/deskstudy.svg'
+import fashionguy from '/src/assets/fashionguy.svg'
+import { DiBootstrap, } from "react-icons/di";
+import { purple } from '@mui/material/colors';
 
 export default function About() {
     return (
@@ -10,13 +15,70 @@ export default function About() {
                     <h1 className='so'>So.. </h1>
                 </div>
                 <img className='chopper' src={chopper} alt="loading..." />
-                <h1 className='who-am-i'>What <span className='Purple'>about me</span>?</h1>
+                <h1 className='who-am-i'>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'What about me?',
+                            2000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Who am I?',
+                            2000,
+                            'Get to know me!',
+                            2000
+                        ]}
+                        wrapper="span"
+                        speed={20}
+                        style={{ display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                </h1>
+
                 <h2>Hey!
                     <br />
                     My name is <span className='Purple'>Tristan Bartolome</span> from <span className='Purple'>Sydney, Australia.</span>
                     <br />
                     I'm a huge fan of anime and gaming, and you'll often find me exploring the latest trends in fashion. I consider myself to be quite social and I'm always up for a good time and meeting new people.
                 </h2>
+            </div>
+
+            <img className='about-img' src={deskstudy} alt="loading..." />
+            <img className='fashion-guy' src={fashionguy} alt="loading..." />
+
+            <div className="why-coding">
+                <h1 className='Purple'>WHY CODING?</h1>
+                <h2>I'm just a guy that really loves his technology from both a user and developer standpoint!
+                    <br />
+                    What I love about coding is that it's literally what shapes the future of tech, and nothing excites me more than technological advancement.
+                    <br />
+                    I also love that programming is heavily team-based. Through my past experience working in a team of devs has by far been the best team enviroment I have experienced to date, and the joy upon completing a project is the most rewarding feeling a dev could feel!
+                </h2>
+            </div>
+            <div className='break'></div>
+
+            <div className="techstack">
+                <h1>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'MY TECHSTACK',
+                                2000,
+                                'MY FRAMEWORKS',
+                                2000,
+                                'MY TOOLS',
+                                2000,
+                                'MY SKILLS',
+                                2000,
+                                'MY TECHNOLOGIES',
+                                2000
+
+                            ]}
+                            wrapper="span"
+                            speed={10}
+                            style={{ display: 'inline-block' }}
+                            repeat={Infinity}
+                            color={purple}
+                        />
+                </h1>
             </div>
         </div>
     )
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import { TypeAnimation } from 'react-type-animation';
 import techguy from '/src/assets/techguy.svg'
 import kirby from '/src/assets/kirby.gif'
 import officedesk from '/src/assets/officedesk.svg'
@@ -14,9 +15,30 @@ export default function Home() {
                 </div>
                 <img className='kirby' src={kirby} alt="loading..." />
                 <h1>My name is <span className='Purple'>Tristan</span></h1>
-                <h2 className='Purple'>JUNIOR SOFTWARE ENGINEER</h2>
+                <div className="Purple">
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Junior Software Developer',
+                            2000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Full Stack Developer',
+                            2000,
+                            'REACT',
+                            2000,
+                            'JavaScript',
+                            2000,
+                            'Ruby On Rails',
+                            2000
+                        ]}
+                        wrapper="span"
+                        speed={20}
+                        style={{ fontSize: '2em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                </div>
                 <h2>Take a look around my website if you would like to get to know a bit more about me!</h2>
             </div>
+
             <img className='home-img' src={techguy} alt="loading..." />
             <img className='intro-img' src={officedesk} alt="loading..." />
 
@@ -24,9 +46,9 @@ export default function Home() {
                 <h1 className='Purple'>HOW ABOUT AN INTRODUCTION?</h1>
                 <h2>Hey! I'm <span className='Purple'>Tristan</span>, or <span className='Purple'>Tris</span> for short.
                     <br />
-                    I'm a software dev that has recently graduated from <span className='Purple'>General Assembly's Software Engineering Immersive</span> course. 
-                    <br /> 
-                    During My time at <span className='Purple'>GA</span>, my passion for coding has grown exponentially. Upon first learning how to code, it felt like stepping into a new world, one that has never been explored (by me at least). 
+                    I'm a software dev that has recently graduated from <span className='Purple'>General Assembly's Software Engineering Immersive</span> course.
+                    <br />
+                    During My time at <span className='Purple'>GA</span>, my passion for coding has grown exponentially. Upon first learning how to code, it felt like stepping into a new world, one that has never been explored (by me at least).
                     <br />
                     With every new line of code or function that I write, it feels like discovering a new landmark!
                     <br />
